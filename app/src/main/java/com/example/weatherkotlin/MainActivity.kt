21c6d672bfd786b02e7cd87ed68b255e865c55d6
FaraKrik <googi13011@gmail.com>
2022-05-13 18:33:37 +0300

    weatherKotlinHomeWork 9

--- app/src/main/java/com/example/weatherkotlin/MainActivity.kt
+++ app/src/main/java/com/example/weatherkotlin/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
+import com.example.weatherkotlin.ui.main.ContentProviderFragment
 import com.example.weatherkotlin.ui.main.MainFragment
 import com.example.weatherkotlin.ui.main.history.HistoryFragment
 import com.example.weatherkotlin.ui.main.threads.ThreadsFragment
@@ -36,6 +37,10 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.menu_history -> {
                 openFragment(HistoryFragment.newInstance())
+                true
+            }
+            R.id.menu_content_provider -> {
+                openFragment(ContentProviderFragment.newInstance())
                 true
             }
             else -> super.onOptionsItemSelected(item)
